4839688e56fda3405c1c883178d64c05156e5581
wessam aftab <wisamkayani360@gmail.com>
2024-04-18 18:24:57 +0500

    ok

---
+++ level -2/pintrest/script.js
@@ -0,0 +1,20 @@
+// let a = document.getElementById("home");
+// const randColor = () =>  {
+//     return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+// }
+// let abc = () =>{
+//     // if(a.style.color != "red"){
+//         a.style.color = randColor();
+// console.log(color);
+    // a.style.color = ;
+    // }
+    // else{
+    //     a.style.color = "green"
+    // }
+
+// }
+let dat = document.getElementById("about").innerHTML;
+let clicked = () =>{
+console.log(dat);
+console.log(15152);
+}
